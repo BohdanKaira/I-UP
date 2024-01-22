11722776734f696474fee0ed45534960a36a32dd
BohdanKaira <bogdankaira0404@gmail.com>
2024-01-22 18:31:51 +0200

    complete accordion

--- assets/js/dist/main.dev.js
+++ assets/js/dist/main.dev.js
@@ -1,30 +1,54 @@
 "use strict";
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-var btn = document.querySelectorAll(".accordion__btn");
-btn.forEach(function (i) {
-  i.addEventListener("click", function () {
-    var parent = i.parentNode;
+function Accordion() {
+  var btn = document.querySelectorAll(".accordion__btn");
+  btn.forEach(function (i) {
+    i.addEventListener("click", function () {
+      var parent = i.parentNode;
 
-    if (parent.classList.contains("active")) {
-      parent.classList.remove("active");
-    } else {
-      document.querySelectorAll(".accordion__item").forEach(function (child) {
-        return child.classList.remove("active");
-      });
-      parent.classList.add("active");
-    }
+      if (parent.classList.contains("active")) {
+        parent.classList.remove("active");
+      } else {
+        document.querySelectorAll(".accordion__item").forEach(function (child) {
+          return child.classList.remove("active");
+        });
+        parent.classList.add("active");
+      }
+    });
   });
-});
+}
+
+Accordion();
+
+function toggleAccordion(accordionId) {
+  document.querySelectorAll('.accordion').forEach(function (accordion) {
+    accordion.classList.remove('active_accordion');
+  });
+  document.getElementById("".concat(accordionId, "Accordion")).classList.add('active_accordion');
+}
+
+function toggleButton(buttonId) {
+  document.querySelectorAll('.accordion_filter_button').forEach(function (button) {
+    button.classList.remove('active_button');
+  });
+  var clickedButton = document.getElementById(buttonId);
+  clickedButton.classList.add('active_button');
+} // function toggleButton(buttonId) {
+//     var button1 = document.getElementsByClassName('venturebutton');
+//     var button2 = document.getElementById('qualifybutton');
+//     var clickedButton = document.getElementById(buttonId);
+//     clickedButton.classList.add('active_button');
+//     if (buttonId === 'venturebutton') {
+//         button2.classList.remove('active_button');
+//     } else if (buttonId === 'qualifybutton') {
+//         button1.classList.remove('active_button');
+//     }
+//     console.log(23);
+// }
+// function toggleClass(buttonId) {
+//     var clickedButton = document.getElementById(buttonId);
+//     clickedButton.classList.add('active_button');
+//     var otherButtonId = (buttonId === 'venturebutton') ? 'qualifybutton' : 'venturebutton';
+//     var otherButton = document.getElementById(otherButtonId);
+//     otherButton.classList.remove('active_button');
+// }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,66 @@
-// var acc = document.getElementsByClassName("accordion");
-// var i;
+function Accordion() {
+    const btn = document.querySelectorAll(".accordion__btn");
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
+    btn.forEach(i => {
+    i.addEventListener("click", function() {
+        const parent = i.parentNode;
 
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
+        if (parent.classList.contains("active")) {
+        parent.classList.remove("active")
+        } else {
+        document.querySelectorAll(".accordion__item").forEach((child) => child.classList.remove("active"))
+        parent.classList.add("active")
+        }
+    })
+    })
+}
+Accordion();
+
+function toggleAccordion(accordionId) {
+   
+    document.querySelectorAll('.accordion').forEach(accordion => {
+        accordion.classList.remove('active_accordion');
+    });
+    document.getElementById(`${accordionId}Accordion`).classList.add('active_accordion');
+
+}
+function toggleButton(buttonId) {
+    
+    document.querySelectorAll('.accordion_filter_button').forEach(button => {
+        button.classList.remove('active_button');
+    });
+
+    
+    var clickedButton = document.getElementById(buttonId);
+    clickedButton.classList.add('active_button');
+}
+// function toggleButton(buttonId) {
+    
+//     var button1 = document.getElementsByClassName('venturebutton');
+//     var button2 = document.getElementById('qualifybutton');
+
+    
+//     var clickedButton = document.getElementById(buttonId);
+
+    
+//     clickedButton.classList.add('active_button');
+
+    
+//     if (buttonId === 'venturebutton') {
+//         button2.classList.remove('active_button');
+//     } else if (buttonId === 'qualifybutton') {
+//         button1.classList.remove('active_button');
 //     }
-//   });
+//     console.log(23);
 // }
-const btn = document.querySelectorAll(".accordion__btn");
+// function toggleClass(buttonId) {
+//     var clickedButton = document.getElementById(buttonId);
 
-btn.forEach(i => {
-  i.addEventListener("click", function() {
-    const parent = i.parentNode;
+    
+//     clickedButton.classList.add('active_button');
 
-    if (parent.classList.contains("active")) {
-      parent.classList.remove("active")
-    } else {
-      document.querySelectorAll(".accordion__item").forEach((child) => child.classList.remove("active"))
-      parent.classList.add("active")
-    }
-  })
-})
+    
+//     var otherButtonId = (buttonId === 'venturebutton') ? 'qualifybutton' : 'venturebutton';
+//     var otherButton = document.getElementById(otherButtonId);
+//     otherButton.classList.remove('active_button');
+// }
